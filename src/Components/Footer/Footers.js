c692b613c5ef94d158f3738f2fa2dd307b73dca4
vishalk31 <vishal.k@pinaqui.com>
2022-11-14 00:46:48 +0530

    footer updated

--- src/Components/Footer/Footers.js
+++ src/Components/Footer/Footers.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-scroll";
 import geulogo from "../../Assets/geu-logo2.png";
 import ieeelogo from "../../Assets/logo3.png";
 import { BsInstagram, BsGithub } from "react-icons/bs";
@@ -9,14 +10,14 @@ import { IoCall } from "react-icons/io5";
 const Footers = () => {
   return (
     <>
-      <section className="w-[100%] h-[full]">
+      <section className="w-[100%] h-[full] border-solid border-t-2 border-black">
         <div className="max-w-[1200px] w-[100%] grid grid-cols-6 mx-auto">
           <div className="col-span-6 sm:col-span-6  md:col-span-2 flex justify-center items-center">
             <div className="m-2">
               <img src={geulogo} alt="" className="h-[80px] w-[80px]" />
             </div>
             <div className="m-2">
-              <img src={ieeelogo} alt="" className="h-[80px] w-[150px]" />
+              <img src={ieeelogo} alt="" className="h-[80px] w-[175px]" />
             </div>
           </div>
           <div className=" col-span-6 sm:col-span-6 md:col-span-1 flex flex-col justify-center items-center p-2">
@@ -24,11 +25,71 @@ const Footers = () => {
               Quick Links
             </h4>
             <ul className="text-[#000] text-[1rem] font-normal p-1 m-2 capitalize">
-              <li className="text-center md:text-left">Home</li>
-              <li className="text-center md:text-left">About us</li>
-              <li className="text-center md:text-left">Events</li>
-              <li className="text-center md:text-left">Gallery</li>
-              <li className="text-center md:text-left">Membership</li>
+              <li className="text-center md:text-left">
+                <Link
+                  activeClass="active"
+                  to="navbar"
+                  spy={true}
+                  smooth={true}
+                  offset={0}
+                  duration={1000}
+                  delay={100}
+                >
+                  Home
+                </Link>
+              </li>
+              <li className="text-center md:text-left">
+                <Link
+                  activeClass="active"
+                  to="about"
+                  spy={true}
+                  smooth={true}
+                  offset={0}
+                  duration={1000}
+                  delay={100}
+                >
+                  About us
+                </Link>
+              </li>
+              <li className="text-center md:text-left">
+                <Link
+                  activeClass="active"
+                  to="event"
+                  spy={true}
+                  smooth={true}
+                  offset={0}
+                  duration={1000}
+                  delay={100}
+                >
+                  Events
+                </Link>
+              </li>
+              <li className="text-center md:text-left">
+                <Link
+                  activeClass="active"
+                  to="gallery"
+                  spy={true}
+                  smooth={true}
+                  offset={0}
+                  duration={1000}
+                  delay={100}
+                >
+                  Gallery
+                </Link>
+              </li>
+              <li className="text-center md:text-left">
+                <Link
+                  activeClass="active"
+                  to="membership"
+                  spy={true}
+                  smooth={true}
+                  offset={0}
+                  duration={1000}
+                  delay={100}
+                >
+                  Membership
+                </Link>
+              </li>
             </ul>
           </div>
           <div className="col-span-6 sm:col-span-6 md:col-span-2 flex flex-col justify-start items-center px-16">
@@ -37,20 +98,28 @@ const Footers = () => {
             </h4>
             <ul className="flex flex-col justify-start items-start text-[#000] text-[1rem] font-normal p-1 mx-5 ">
               <li className="flex  justify-center items-center p-2">
-                <span className="p-1">
+                <span className="p-1 hover:cursor-pointer hover:scale-105">
                   <a
                     href="mailto:ieeegeu.studentbranch@gmail.com"
                     target="_blank"
                     rel="noreferrer"
                   >
-                    <SiGmail size={20} />
+                    <SiGmail size={20} color="#D44638" />
                   </a>
                 </span>
-                <p className="pl-3">ieeegeu.studentbranch@gmail.com</p>
+                <p className="pl-3 hover:cursor-pointer hover:scale-105">
+                  <a
+                    href="mailto:ieeegeu.studentbranch@gmail.com"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    ieeegeu.studentbranch@gmail.com
+                  </a>
+                </p>
               </li>
-              <li className="flex  justify-center items-center p-2">
+              <li className="flex  justify-center items-center p-2 hover:cursor-pointer hover:scale-105">
                 <span className="p-1">
-                  <IoCall size={20} />
+                  <IoCall size={20} color="#28D146" />
                 </span>
                 <p className="pl-3">+91 8456483999</p>
               </li>
@@ -61,38 +130,38 @@ const Footers = () => {
               Follow Us
             </h4>
             <ul className="flex justify-center items-center p-2 m-2">
-              <li className=" p-2 m-1">
+              <li className=" p-2 m-1 hover:cursor-pointer hover:scale-105">
                 <a
                   href="https://www.instagram.com/ieee.geu/"
                   target="_blank"
                   rel="noreferrer"
                 >
-                  <BsInstagram size={20} />
+                  <BsInstagram size={20} color="#C13584" />
                 </a>
               </li>
-              <li className=" p-2 m-1">
+              <li className=" p-2 m-1 hover:cursor-pointer hover:scale-105">
                 <a
                   href="https://www.linkedin.com/in/ieee-geu/"
                   target="_blank"
                   rel="noreferrer"
                 >
-                  <FaLinkedinIn size={20} />
+                  <FaLinkedinIn size={20} color="#0A66C2" />
                 </a>
               </li>
-              <li className=" p-2 m-1">
+              <li className=" p-2 m-1 hover:cursor-pointer hover:scale-105">
                 <a
                   href="https://github.com/ieeegeu"
                   target="_blank"
                   rel="noreferrer"
                 >
-                  <BsGithub size={20} />
+                  <BsGithub size={20} color="#000" />
                 </a>
               </li>
             </ul>
           </div>
         </div>
         <div className="border-solid border-t-2 border-black">
-          <p className="text-[#000] text-[1rem] font-semibold text-center p-2">
+          <p className="text-[#000] text-[1rem] font-semibold text-center p-2 uppercase">
             Copyright@IEEE-2022
           </p>
         </div>
