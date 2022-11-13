a23ecc1e0ada0eeafa75ae9f3393d96bf2a5c2bc
vishalk31 <vishal.k@pinaqui.com>
2022-11-13 23:49:43 +0530

    developed gallery components

---
+++ src/Components/Data/MainGalleryData.js
@@ -0,0 +1,65 @@
+import g1 from "../../Assets/g1.jpg";
+import g2 from "../../Assets/g2.jpg";
+import g3 from "../../Assets/g3.jpg";
+import g4 from "../../Assets/g4.jpg";
+
+const MainGalleryData = [
+  {
+    id: 1,
+    imgSrc: g1,
+    width: 300,
+    height: 450,
+    category: "utsah",
+  },
+  {
+    id: 2,
+    imgSrc: g2,
+    width: 400,
+    height: 250,
+    category: "kindle",
+  },
+  {
+    id: 3,
+    imgSrc: g3,
+    width: 450,
+    height: 300,
+    category: "expo",
+  },
+  {
+    id: 4,
+    imgSrc: g4,
+    width: 300,
+    height: 200,
+    category: "conclave",
+  },
+  {
+    id: 5,
+    imgSrc: g1,
+    width: 300,
+    height: 450,
+    category: "utsah",
+  },
+  {
+    id: 6,
+    imgSrc: g2,
+    width: 400,
+    height: 250,
+    category: "kindle",
+  },
+  {
+    id: 7,
+    imgSrc: g3,
+    width: 450,
+    height: 300,
+    category: "expo",
+  },
+  {
+    id: 8,
+    imgSrc: g4,
+    width: 300,
+    height: 200,
+    category: "conclave",
+  },
+];
+
+export default MainGalleryData;
--- src/Components/Gallery/MainGallery.js
+++ src/Components/Gallery/MainGallery.js
@@ -1,7 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
+import { MdKeyboardArrowUp, MdOutlineKeyboardArrowDown } from "react-icons/md";
+import MainGalleryData from "../Data/MainGalleryData";
 
 const MainGallery = () => {
-  return <></>;
+  const [Items, setItems] = useState(MainGalleryData);
+
+  const filterItems = (categoryItems) => {
+    if (categoryItems === "all") {
+      setItems(MainGalleryData);
+    } else {
+      const updatedItems = MainGalleryData.filter((curElems) => {
+        return curElems.category === categoryItems;
+      });
+      setItems(updatedItems);
+    }
+  };
+
+  return (
+    <section className="max-w-[1200px] w-[100%] h-full mx-auto">
+      <h2 className="text-5xl text-sky-600 font-bold text-center uppercase p-5 mt-5 mb-10 ">
+        Gallery
+      </h2>
+      <div className="flex flex-col justify-center items-center">
+        <div className="w-[100%] h-[40px] md:h-[50px] flex  justify-center items-center ">
+          <select
+            name=""
+            id=""
+            className="w-[250px] md:w-[600px] h-[40px] md:h-[50px] text-center font-medium text-[1rem] md:text-[1.2rem] uppercase border-solid border-2 border-black"
+            onChange={(e) => filterItems(e.target.value)}
+          >
+            <option
+              value="all"
+              className="text-center font-medium text-[1rem] md:text-[1.2rem] uppercase hover:cursor-pointer border-solid border-b-2 border-black p-3"
+            >
+              ALL
+            </option>
+            <option
+              value="utsah"
+              className="text-center font-medium text-[1rem] md:text-[1.2rem] uppercase hover:cursor-pointer border-solid border-b-2 border-black p-3"
+            >
+              Utsah
+            </option>
+            <option
+              value="conclave"
+              className="text-center font-medium text-[1rem] md:text-[1.2rem] uppercase hover:cursor-pointer border-solid border-b-2 border-black p-3"
+            >
+              Research conclave
+            </option>
+            <option
+              value="expo"
+              className="text-center font-medium text-[1rem] md:text-[1.2rem] uppercase hover:cursor-pointer border-solid border-b-2 border-black p-3"
+            >
+              Research Expo
+            </option>
+            <option
+              value="kindle"
+              className="text-center font-medium text-[1rem] md:text-[1.2rem] uppercase hover:cursor-pointer border-solid border-black p-3"
+            >
+              Kindle 1.0
+            </option>
+          </select>
+        </div>
+      </div>
+      <div className="w-[100%] my-10 grid grid-cols-4">
+        <div className="col-span-1">
+          {Items.map((val, ind) => {
+            return (
+              <div key={val.id} className="p-2 block  col-span-1">
+                <img src={val.imgSrc} alt={val.category} className="" />
+              </div>
+            );
+          })}
+        </div>
+        <div className="col-span-2">
+          {Items.map((val, ind) => {
+            return (
+              <div key={val.id} className="p-2 block  col-span-1">
+                <img
+                  src={val.imgSrc}
+                  alt={val.category}
+                  className=" w-[100%] h-[50%]"
+                />
+              </div>
+            );
+          })}
+        </div>
+        <div className="col-span-1">
+          {Items.map((val, ind) => {
+            return (
+              <div key={val.id} className="p-2 block  col-span-1">
+                <img src={val.imgSrc} alt={val.category} className="" />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default MainGallery;
